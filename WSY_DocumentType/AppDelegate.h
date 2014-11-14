699326fec5008e6c8f5ef7dcd11dca361f3c0bb4
yuanshichong <yuanshichong@shanchuan.cn>
2014-11-14 18:38:24 +0800

    FirstCommit

---
+++ WSY_DocumentType/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSY_DocumentType
+//
+//  Created by Andou on 14/11/14.
+//  Copyright (c) 2014年 SCDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSY_DocumentType/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSY_DocumentType
+//
+//  Created by Andou on 14/11/14.
+//  Copyright (c) 2014年 SCDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
